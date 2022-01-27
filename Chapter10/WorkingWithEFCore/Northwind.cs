2b63b3abc901ffe8b4e636d809bcfa246e0f1f7a
emmygreatmusic <96184388+emmygreatmusic@users.noreply.github.com>
2022-01-27 12:48:43 +0100

    Add files via upload

---
+++ Chapter10/WorkingWithEFCore/Northwind.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using static System.Console;
+
+namespace Packt.Shared;
+internal class Northwind : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (ProjectConstants.DatabaseProvider == "SQLite")
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, "Northwind.db");
+            WriteLine($"using {path} database file.");
+            optionsBuilder.UseSqlite($"Filename={path}");
+        }
+        else
+        {
+            string connection = "Data Source =.;" + "Initial Catalog = Northwind;"
+        }
+    }
+}
---
+++ Chapter10/WorkingWithEFCore/Program.cs
@@ -0,0 +1,5 @@
+﻿using static System.Console;
+
+using Packt.Shared;
+
+WriteLine($"Using {ProjectConstants.DatabaseProvider} database provider.");
